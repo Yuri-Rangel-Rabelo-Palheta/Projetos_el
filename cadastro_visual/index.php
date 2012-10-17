9db5d3ad571b29fc34199baa62417ec5339fc54e
Genival Jr <moreno.alado@gmail.com>
2012-10-17 09:33:10 -0300

    Criando Cadastro Visual
    Yuri, cuidado com o q vc upa. Primeiro "puxa", antes de alterar.

---
+++ cadastro_visual/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Clube de RPG Espirito Livre - Cadastro</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+  	<div class="container-fluid">
+    	<div class="navbar navbar-inverse navbar-fixed-top">
+        	<div class="navbar-inner">
+            	<div class="container">
+                	<div class="nav-collapse collapse">
+                    	<ul class="nav">
+                        	<li class="active"><a href="">Home</a></li>
+                        	<li><a href="">Quem Somos</a></li>
+                        	<li><a href="">Contato</a></li>
+                        </ul>
+                        <div class="span5 offset3">
+	                        <form class="form-inline">
+		                        <label for="">Login:</label>
+		                        <input type="text" class="input-medium" placeholder="Email">
+		                        <input type="text" class="input-medium" placeholder="Senha">                        
+	                        </form>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+	            <div class="row">
+	            	<div class="span8"><img src="img/logo_g.jpg" alt=""></div>
+	                <div class="span4">
+	                	<form action="">
+	                    	<legend>Cadastra-se.</legend>
+	                        
+	                    </form>
+	                </div>
+	            
+	            </div>
+        	</div>
+        </div>
+    
+    </div>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
