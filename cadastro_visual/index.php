046bebc6dfb37e3bb791e260ed26578a175bb656
Genival dos Santos Furtado Junior <moreno.alado@gmail.com>
2012-10-17 00:05:16 -0300

    Iniciando a Página Incial

--- cadastro_visual/index.php
+++ cadastro_visual/index.php
@@ -1,12 +1,45 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>Clube de RPG Espirito Livre - Cadastro</title>
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  	<div class="container-fluid">
+    	<div class="navbar navbar-inverse navbar-fixed-top">
+        	<div class="navbar-inner">
+            	<div class="container">
+                	<div class="nav-collapse collapse">
+                    	<ul class="nav">
+                        	<li class="active"><a href="">Home</a></li>
+                        	<li><a href="">Quem Somos</a></li>
+                        	<li><a href="">Contato</a></li>
+                        </ul>
+                        <div class="span5 offset3">
+	                        <form class="form-inline">
+		                        <label for="">Login:</label>
+		                        <input type="text" class="input-medium" placeholder="Email">
+		                        <input type="text" class="input-medium" placeholder="Senha">                        
+	                        </form>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+            	<div class="span8"></div>
+                <div class="span4">
+                	<form action="">
+                    	<legend>Cadastra-se.</legend>
+                        
+                    </form>
+                </div>
+            
+            </div>
+        </div>
+    
+    </div>
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </body>
