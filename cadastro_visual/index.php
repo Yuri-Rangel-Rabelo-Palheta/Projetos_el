92a6fbec140eb800e146a2c149e4c95ac2c97c3f
Genival dos Santos Furtado Junior <moreno.alado@gmail.com>
2012-10-18 00:19:15 -0300

    Intro e Origem, Cadastro iniciado.

--- cadastro_visual/index.php
+++ cadastro_visual/index.php
@@ -1,47 +1,96 @@
 <!DOCTYPE html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <html>
   <head>
     <title>Clube de RPG Espirito Livre - Cadastro</title>
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/mod.css">
   </head>
-  <body>
-  	<div class="container-fluid">
-    	<div class="navbar navbar-inverse navbar-fixed-top">
+  <body data-spy="scroll" data-target=".navbar">
+    	<div class="navbar navbar-fixed-top">
         	<div class="navbar-inner">
             	<div class="container">
                 	<div class="nav-collapse collapse">
                     	<ul class="nav">
-                        	<li class="active"><a href="">Home</a></li>
-                        	<li><a href="">Quem Somos</a></li>
-                        	<li><a href="">Contato</a></li>
+                        	<li><a href="#home">Home</a></li>
+                        	<li><a href="#origem">A Origem</a></li>
+                        	<li><a href="#cadastro">Cadastro</a></li>
+                            <li><a href="">Contato</a></li>
                         </ul>
-                        <div class="span5 offset3">
-	                        <form class="form-inline">
-		                        <label for="">Login:</label>
-		                        <input type="text" class="input-medium" placeholder="Email">
-		                        <input type="text" class="input-medium" placeholder="Senha">                        
-	                        </form>
-                        </div>
-                        
+                    <a href="" id="titulo_topo" class="brand">Espirito Livre - Clube de RPG</a>                        
                     </div>
                 </div>
             </div>
-            <div class="container">
-	            <div class="row">
-	            	<div class="span8"><img src="img/logo_g.jpg" alt=""></div>
-	                <div class="span4">
-	                	<form action="">
-	                    	<legend>Cadastra-se.</legend>
-	                        
-	                    </form>
-	                </div>
-	            
-	            </div>
-        	</div>
         </div>
-    
-    </div>
+	    <div id="home" class="container">
+			<div class="row">
+				<div class="span7"><img src="img/logo_g.jpg" alt=""></div>
+				<div class="span5">
+	                <div id="home2" class="row">
+                    	<h2>Vamos jogar RPG?</h2>   
+	                    <p>
+	                    O <strong>Clube de RPG Espírito Livre</strong> é uma iniciativa que visa reunir mestres/narradores/jogadores de RPG de todo o Brasil, organizados com um único objetivo: fazer a cultura RPGista crescer.
+                        </p>
+                        <p> Através da realização de eventos, parcerias com editoras e livrarias, sorteios, aquisição de novos espaços para a prática do RPG, e principalmente, a atração de cada vez mais novos praticantes de nosso adorado hobby, vamos fazer do Clube de RPG Espírito Livre uma referência não apenas nacional, mas talvez para além de nossas fronteiras...afinal, qual o limite para quem tem Espírito Livre? 
+                       </p>
+                       <p>Venha conosco em aventuras cada vez mais distantes, porque só a imaginação é nosso limite! <br>
+	                    </p>
+	                </div>
+                    <br><br>
+                    <div class="row">
+						<blockquote class="pull-right">
+							<p><em>
+								"Sozinhos somos poderosos.
+								Juntos, somos lendas."
+							</em></p>
+							<small>Slogan do jogo X-Men Legends 2*</small>
+						</blockquote>
+                    </div>
+				</div>
+			</div>
+		</div>
+        <div id="origem" class="container">
+        	<div class="row">
+                <div class="span6">
+                <h2>A Origem</h2>
+                	<p>
+                    A alguns anos atrás, em 2007 salvo engano, 4 jovens iniciaram uma pequena missão; em uma breve jornada por mundos além da compreensão, encontraram algo pequeno e de pouco poder, mas com um enorme potencial por se desenvolver; não era uma pedra mágica, uma espada sagrada, tampouco uma armadura mágica. O que tinham em mãos era simplesmente uma ideia...mas não seriam as ideias origem do verdadeiro poder?
+Nascia ali o Espírito Livre, um nome, um grupo, uma reunião de aventureiros, uma ideia. Esse nascimento era especial porque, os jovens mesmos não sabiam o nível do elemento que agora guardavam; não sabiam eles que dentro daquela coisa simples um poder imenso estava adormecido...
+                    </p>
+                    <p>
+                    <em>Curioso? Em breve, mais detalhes da nossa história...</em>
+                    </p>
+                </div>
+            	<div class="span6">
+                	<center><img src="img/mesa-de-rpg.jpg" alt=""></center>
+                </div>
+            </div>
+        </div>
+        <div id="cadastro" class="container">
+        	<div class="row">
+            	<div class="span6">
+                
+                </div>
+            	<div class="span6">
+                	<h2>Cadastre-se</h2>
+                    <p>
+                    Entrando no clube você tem muitas vantagens, que se dividem no seu nivel de compromentimento:
+                    </p>
+                    <p>
+                    <h4>Associado Simples</h4>
+                    Participará como playtester dos nossos aplicativo nacional, associado ao <strong>Facebook</strong> com sistema de gerencia de mesas, avaliação, pontuação e muito mais...
+                    </p>
+                    <p>
+                    <h4>Associado VIP</h4>
+                    Pelo valor <strong>anual</strong> de 15 reais, desfrutará de desconto na compra de material nas editoras/lojas parceiras, sorteios de material e narradores terão brindes garantidos nos eventos do clube. No futuro, desconto em filmes e eventos do meio nerd/geek/otaku. <br>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <footer>
+        
+        </footer>
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </body>
