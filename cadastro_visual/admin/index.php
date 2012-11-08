b8e620cbe5f9692fb4559f93a0b7200e5e8bfa6a
Genival Jr <moreno.alado@gmail.com>
2012-11-08 16:35:03 -0300

    Criando área administrativa

---
+++ cadastro_visual/admin/index.php
@@ -0,0 +1,15 @@
+<?php include("../system/funcoes.php"); ?>
+<!DOCTYPE html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<html>
+  <head>
+    <title>Clube de RPG Espirito Livre - Administração</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/mod.css">
+  </head>
+
+<body>
+asdasd
+</body>
+</html>
